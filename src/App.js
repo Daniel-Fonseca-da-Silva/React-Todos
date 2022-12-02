0b93d7bf304505d23d6e839c7f2881e2fa131fb6
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-12-02 01:20:04 -0300

    feat: Creating React Project Template.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          We changed this with the power of react
-        </p>
+    <div>
+      <div>
+        <div>
+          You Todo's
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>Feed dog</td>
+                <td>Eric</td>
+              </tr>
+              <tr>
+                <th scope='row'>2</th>
+                <td>Get haircut</td>
+                <td>Eric</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
