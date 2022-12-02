c83c0fe0de0c468263236bb6696e2296fb26a835
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-12-02 02:35:08 -0300

    feat: Passing Data To React Components Dynamically.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import './App.css';
 import TodoRowItem from './components/TodoRowItem';
 
 function App() {
+
+  const todos = [
+    {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User one'},
+    {rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User two'},
+    {rowNumber: 3, rowDescription: 'Make dinner', rowAssigned: 'User three'}
+  ]
+
   return (
     <div className='mt-5 container'>
       <div className='card'>
@@ -18,13 +25,18 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              <TodoRowItem/>
-              <TodoRowItem/>
-              <tr>
-                <th scope='row'>2</th>
-                <td>Get haircut</td>
-                <td>Eric</td>
-              </tr>
+              <TodoRowItem
+                rowNumber={todos[0].rowNumber}
+                rowDescription={todos[0].rowDescription}
+                rowAssigned={todos[0].rowAssigned}/>
+                <TodoRowItem
+                rowNumber={todos[1].rowNumber}
+                rowDescription={todos[1].rowDescription}
+                rowAssigned={todos[1].rowAssigned}/>
+                <TodoRowItem
+                rowNumber={todos[2].rowNumber}
+                rowDescription={todos[2].rowDescription}
+                rowAssigned={todos[2].rowAssigned}/>
             </tbody>
           </table>
         </div>
