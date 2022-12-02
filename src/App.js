44dfcd227d3b0d1b29fe3aebeb1ba66198f6b850
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-12-02 02:00:54 -0300

    feat: Creating a New React Component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import TodoRowItem from './components/TodoRowItem';
 
 function App() {
   return (
@@ -17,11 +18,7 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope='row'>1</th>
-                <td>Feed dog</td>
-                <td>Eric</td>
-              </tr>
+              <TodoRowItem/>
               <tr>
                 <th scope='row'>2</th>
                 <td>Get haircut</td>
---
+++ src/components/TodoRowItem.js
@@ -0,0 +1,11 @@
+function TodoRowItem() {
+   return (
+    <tr>
+        <th scope="row">1</th>
+        <td>Feed dog</td>
+        <td>Eric</td>
+    </tr>
+   )
+}
+
+export default TodoRowItem
