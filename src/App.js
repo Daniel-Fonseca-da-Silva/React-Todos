c0c9ab53b49314e116d32c41d1bd586c62376a44
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-12-04 23:12:37 -0300

    feat: Creating React Form Inputs.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoForm';
 
 function App() {
 
@@ -34,6 +35,7 @@ function App() {
           <button className="btn btn-primary" onClick={addTodo} >
             Add new todo
             </button>
+            <NewTodoForm/>
         </div>
       </div>
     </div>
